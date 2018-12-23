d32914267fe9e5c776b443483b635de6635eed16
Simon Olander Sahlén <simon.olander@gmail.com>
2018-12-23 18:12:33 +0100

    Added instruction images for branch, add and subtract

--- src/InstructionView.elm
+++ src/InstructionView.elm
@@ -34,5 +34,113 @@ view attributes instruction =
                 , description = ""
                 }
 
+        Branch Left Left -> 
+            image attributes
+                { src = "assets/instruction-images/branch-left-left.svg"
+                , description = ""
+                }
+
+        Branch Left Up -> 
+            image attributes
+                { src = "assets/instruction-images/branch-left-up.svg"
+                , description = ""
+                }
+
+        Branch Left Right -> 
+            image attributes
+                { src = "assets/instruction-images/branch-left-right.svg"
+                , description = ""
+                }
+
+        Branch Left Down -> 
+            image attributes
+                { src = "assets/instruction-images/branch-left-down.svg"
+                , description = ""
+                }
+
+        Branch Up Left -> 
+            image attributes
+                { src = "assets/instruction-images/branch-up-left.svg"
+                , description = ""
+                }
+
+        Branch Up Up -> 
+            image attributes
+                { src = "assets/instruction-images/branch-up-up.svg"
+                , description = ""
+                }
+
+        Branch Up Right -> 
+            image attributes
+                { src = "assets/instruction-images/branch-up-right.svg"
+                , description = ""
+                }
+
+        Branch Up Down -> 
+            image attributes
+                { src = "assets/instruction-images/branch-up-down.svg"
+                , description = ""
+                }
+
+        Branch Right Left -> 
+            image attributes
+                { src = "assets/instruction-images/branch-right-left.svg"
+                , description = ""
+                }
+
+        Branch Right Up -> 
+            image attributes
+                { src = "assets/instruction-images/branch-right-up.svg"
+                , description = ""
+                }
+
+        Branch Right Right -> 
+            image attributes
+                { src = "assets/instruction-images/branch-right-right.svg"
+                , description = ""
+                }
+
+        Branch Right Down -> 
+            image attributes
+                { src = "assets/instruction-images/branch-right-down.svg"
+                , description = ""
+                }
+
+        Branch Down Left -> 
+            image attributes
+                { src = "assets/instruction-images/branch-down-left.svg"
+                , description = ""
+                }
+
+        Branch Down Up -> 
+            image attributes
+                { src = "assets/instruction-images/branch-down-up.svg"
+                , description = ""
+                }
+
+        Branch Down Right -> 
+            image attributes
+                { src = "assets/instruction-images/branch-down-right.svg"
+                , description = ""
+                }
+
+        Branch Down Down -> 
+            image attributes
+                { src = "assets/instruction-images/branch-down-down.svg"
+                , description = ""
+                }
+
+        Add -> 
+            image attributes
+                { src = "assets/instruction-images/add.svg"
+                , description = ""
+                }
+
+        Subtract -> 
+            image attributes
+                { src = "assets/instruction-images/subtract.svg"
+                , description = ""
+                }
+
         _ ->
             el attributes (text (Debug.toString instruction))
