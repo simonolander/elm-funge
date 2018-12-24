5fe7d2fc83dbe1d8da6283854b1da5f109d74754
Simon Olander Sahlén <simon.olander@gmail.com>
2018-12-24 13:21:57 +0100

    Added execution control icons

---
+++ src/ExecutionControlView.elm
@@ -0,0 +1,46 @@
+module ExecutionControlView exposing (ExecutionControlInstruction(..), view)
+
+import Element exposing (..)
+import Model exposing (..)
+
+
+type ExecutionControlInstruction
+    = Step
+    | Undo
+    | Play
+    | FastForward
+    | Pause
+
+
+view : List (Attribute msg) -> ExecutionControlInstruction -> Element msg
+view attributes instruction =
+    case instruction of
+        Step ->
+            image attributes
+                { src = "assets/execution-control-images/step.svg"
+                , description = "Step"
+                }
+
+        Undo ->
+            image attributes
+                { src = "assets/execution-control-images/undo.svg"
+                , description = "Step Back"
+                }
+
+        Play ->
+            image attributes
+                { src = "assets/execution-control-images/play.svg"
+                , description = "Play"
+                }
+
+        FastForward ->
+            image attributes
+                { src = "assets/execution-control-images/fast-forward.svg"
+                , description = "Fast Forward"
+                }
+
+        Pause ->
+            image attributes
+                { src = "assets/execution-control-images/pause.svg"
+                , description = "Pause"
+                }
