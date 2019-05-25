ed688cf8052f96fe591097ece79f7d1797603d63
Simon Olander <simon.olander@r2m.se>
2019-05-25 17:24:26 +0200

    Ugly high scores

--- src/View/HighScore.elm
+++ src/View/HighScore.elm
@@ -1,6 +1,7 @@
 module View.HighScore exposing (view)
 
 import Data.HighScore exposing (HighScore)
+import Dict
 import Element exposing (..)
 import Element.Font as Font
 import Http exposing (Error(..))
@@ -11,6 +12,25 @@ import View.Constant exposing (color)
 
 view : WebData HighScore -> Element msg
 view remoteData =
+    --    let
+    --        remoteData =
+    --            RemoteData.Success
+    --                { levelId = "12380f983038a019"
+    --                , numberOfSteps =
+    --                    Dict.fromList
+    --                        [ ( 0, 51 )
+    --                        , ( 1, 34 )
+    --                        , ( 6, 13 )
+    --                        , ( 9, 57 )
+    --                        ]
+    --                , numberOfInstructions =
+    --                    Dict.fromList
+    --                        [ ( 0, 7 )
+    --                        , ( 1, 94 )
+    --                        , ( 9, 45 )
+    --                        ]
+    --                }
+    --    in
     case remoteData of
         RemoteData.NotAsked ->
             notAsked
@@ -27,10 +47,61 @@ view remoteData =
 
 success : HighScore -> Element msg
 success highScore =
-    paragraph
-        [ Font.center ]
-        [ text "Loaded high scores" ]
-        |> View.Box.nonInteractive
+    if Dict.isEmpty highScore.numberOfInstructions then
+        View.Box.simpleNonInteractive "No high scores"
+
+    else
+        let
+            numberOfStepsView =
+                highScore.numberOfSteps
+                    |> Dict.toList
+                    |> List.sortBy Tuple.first
+                    |> List.map
+                        (\( key, value ) ->
+                            row
+                                [ width fill
+                                , spacing 20
+                                ]
+                                [ text (String.fromInt key)
+                                , text (String.fromInt value)
+                                ]
+                        )
+                    |> (::)
+                        (paragraph []
+                            [ text "Number of steps:"
+                            ]
+                        )
+                    |> column
+                        [ width fill ]
+
+            numberOfInstructionsView =
+                highScore.numberOfInstructions
+                    |> Dict.toList
+                    |> List.sortBy Tuple.first
+                    |> List.map
+                        (\( key, value ) ->
+                            row
+                                [ width fill
+                                , spacing 20
+                                ]
+                                [ text (String.fromInt key)
+                                , text (String.fromInt value)
+                                ]
+                        )
+                    |> (::)
+                        (paragraph []
+                            [ text "Number of instructions:"
+                            ]
+                        )
+                    |> column
+                        [ width fill ]
+        in
+        [ numberOfInstructionsView
+        , numberOfStepsView
+        ]
+            |> column
+                [ spacing 20 ]
+            |> View.Box.nonInteractive
 
 
 loading : Element msg
