89da8f1e787f37a20086de23b60b0a64ddd6200a
Simon Olander <simon.olander@r2m.se>
2019-06-06 11:16:23 +0200

    Changed deploy script to have proper locations for source

--- google-cloud/deploy.sh
+++ google-cloud/deploy.sh
@@ -4,13 +4,8 @@ cd "$(dirname "$0")"
 cd functions/firestore-function
 npm install
 tsc
-
-cp -r node_modules build/
-cp package.json build/
-cp package-lock.json build/
-
-gcloud functions deploy drafts --runtime nodejs8 --trigger-http --source build
-gcloud functions deploy solutions --runtime nodejs8 --trigger-http --source build
-gcloud functions deploy highScores --runtime nodejs8 --trigger-http --source build
-gcloud functions deploy levels --runtime nodejs8 --trigger-http --source build
-gcloud functions deploy userInfo --runtime nodejs8 --trigger-http --source build
+gcloud functions deploy drafts --runtime nodejs8 --trigger-http
+gcloud functions deploy solutions --runtime nodejs8 --trigger-http
+gcloud functions deploy highScores --runtime nodejs8 --trigger-http
+gcloud functions deploy levels --runtime nodejs8 --trigger-http
+gcloud functions deploy userInfo --runtime nodejs8 --trigger-http
