652efe3468890cc5df3c24b0a175df008c4f68b4
Simon Olander <simon.olander@r2m.se>
2019-08-31 15:46:16 +0100

    Added resource files

---
+++ google-cloud/functions/src/test/resources/levels/index.ts
@@ -0,0 +1,51 @@
+import _1a3c6d6a80769a07 from "./1a3c6d6a80769a07.json";
+import _1fac7ddba473e99d from "./1fac7ddba473e99d.json";
+import _24c7efb5c41f8f8f from "./24c7efb5c41f8f8f.json";
+import _36ae04449442c355 from "./36ae04449442c355.json";
+import _3ee1f15ae601fc94 from "./3ee1f15ae601fc94.json";
+import _407410b1638112a9 from "./407410b1638112a9.json";
+import _42cdf083b26bb8ab from "./42cdf083b26bb8ab.json";
+import _42fe70779bd30656 from "./42fe70779bd30656.json";
+import _519983570eefe19c from "./519983570eefe19c.json";
+import _5ed6d025ab5937e4 from "./5ed6d025ab5937e4.json";
+import _81101cdad21a4ed2 from "./81101cdad21a4ed2.json";
+import _88c653c6c3a5b5e7 from "./88c653c6c3a5b5e7.json";
+import _9abf854cff37e96b from "./9abf854cff37e96b.json";
+import _b4c862e5dcfb82c1 from "./b4c862e5dcfb82c1.json";
+import _b96e6c12476716a3 from "./b96e6c12476716a3.json";
+import _bc27b58a0cafb0ba from "./bc27b58a0cafb0ba.json";
+import _be13bbdd076a586c from "./be13bbdd076a586c.json";
+import _c2003520d988f8d0 from "./c2003520d988f8d0.json";
+import _d3c077ea5033222c from "./d3c077ea5033222c.json";
+import _e2f96c5345e5f1f6 from "./e2f96c5345e5f1f6.json";
+import _e6d9465e4aacaa0f from "./e6d9465e4aacaa0f.json";
+import _e7d5826a6db19981 from "./e7d5826a6db19981.json";
+import _e81d1f82a8a37103 from "./e81d1f82a8a37103.json";
+import _f8ba39bc9d01ef03 from "./f8ba39bc9d01ef03.json";
+
+export default {
+    "1a3c6d6a80769a07": _1a3c6d6a80769a07,
+    "1fac7ddba473e99d": _1fac7ddba473e99d,
+    "24c7efb5c41f8f8f": _24c7efb5c41f8f8f,
+    "36ae04449442c355": _36ae04449442c355,
+    "3ee1f15ae601fc94": _3ee1f15ae601fc94,
+    "407410b1638112a9": _407410b1638112a9,
+    "42cdf083b26bb8ab": _42cdf083b26bb8ab,
+    "42fe70779bd30656": _42fe70779bd30656,
+    "519983570eefe19c": _519983570eefe19c,
+    "5ed6d025ab5937e4": _5ed6d025ab5937e4,
+    "81101cdad21a4ed2": _81101cdad21a4ed2,
+    "88c653c6c3a5b5e7": _88c653c6c3a5b5e7,
+    "9abf854cff37e96b": _9abf854cff37e96b,
+    "b4c862e5dcfb82c1": _b4c862e5dcfb82c1,
+    "b96e6c12476716a3": _b96e6c12476716a3,
+    "bc27b58a0cafb0ba": _bc27b58a0cafb0ba,
+    "be13bbdd076a586c": _be13bbdd076a586c,
+    "c2003520d988f8d0": _c2003520d988f8d0,
+    "d3c077ea5033222c": _d3c077ea5033222c,
+    "e2f96c5345e5f1f6": _e2f96c5345e5f1f6,
+    "e6d9465e4aacaa0f": _e6d9465e4aacaa0f,
+    "e7d5826a6db19981": _e7d5826a6db19981,
+    "e81d1f82a8a37103": _e81d1f82a8a37103,
+    "f8ba39bc9d01ef03": _f8ba39bc9d01ef03,
+};
---
+++ google-cloud/functions/src/test/resources/solutions/index.ts
@@ -0,0 +1,71 @@
+import _02096d9d171a9f85 from "./02096d9d171a9f85.json";
+import _0347f1d09f2bfbb8 from "./0347f1d09f2bfbb8.json";
+import _102a44c26518d92f from "./102a44c26518d92f.json";
+import _1383d5dd58cdf971 from "./1383d5dd58cdf971.json";
+import _141a44a63c4d9079 from "./141a44a63c4d9079.json";
+import _232ed32d07cb116a from "./232ed32d07cb116a.json";
+import _23c37357690e7368 from "./23c37357690e7368.json";
+import _2a9ff428eeffe011 from "./2a9ff428eeffe011.json";
+import _3016ef8b1b8d8ba1 from "./3016ef8b1b8d8ba1.json";
+import _3763da893380788e from "./3763da893380788e.json";
+import _39086c76adecb8ba from "./39086c76adecb8ba.json";
+import _4019956e289995bf from "./4019956e289995bf.json";
+import _4ecdeb4ea40de830 from "./4ecdeb4ea40de830.json";
+import _4f478390ab105dbc from "./4f478390ab105dbc.json";
+import _5176864f54f7c989 from "./5176864f54f7c989.json";
+import _59aeaa264486467f from "./59aeaa264486467f.json";
+import _611d0f1df4e6cc92 from "./611d0f1df4e6cc92.json";
+import _69fe92265705136a from "./69fe92265705136a.json";
+import _6e5a446733dae78e from "./6e5a446733dae78e.json";
+import _7827252d71dd26be from "./7827252d71dd26be.json";
+import _87385e3763cfd6e5 from "./87385e3763cfd6e5.json";
+import _88a0cf903b3fae98 from "./88a0cf903b3fae98.json";
+import _8d9be7d1350b9410 from "./8d9be7d1350b9410.json";
+import _97620ae6d0cecefa from "./97620ae6d0cecefa.json";
+import _a16a7ef790ddf8b5 from "./a16a7ef790ddf8b5.json";
+import _b056a7102f9a5d80 from "./b056a7102f9a5d80.json";
+import _b0b22b0235a4d1ef from "./b0b22b0235a4d1ef.json";
+import _b8a61184c56b908b from "./b8a61184c56b908b.json";
+import _bef85d570a643bfc from "./bef85d570a643bfc.json";
+import _c8f4ee81076ab36a from "./c8f4ee81076ab36a.json";
+import _d0e5844fffb28bf5 from "./d0e5844fffb28bf5.json";
+import _d9a40836b8b1e98d from "./d9a40836b8b1e98d.json";
+import _ecf1faabfd42c507 from "./ecf1faabfd42c507.json";
+import _f0a91b97e78f59d5 from "./f0a91b97e78f59d5.json";
+
+export default {
+    "2a9ff428eeffe011": _2a9ff428eeffe011,
+    "4ecdeb4ea40de830": _4ecdeb4ea40de830,
+    "4f478390ab105dbc": _4f478390ab105dbc,
+    "6e5a446733dae78e": _6e5a446733dae78e,
+    "8d9be7d1350b9410": _8d9be7d1350b9410,
+    "23c37357690e7368": _23c37357690e7368,
+    "59aeaa264486467f": _59aeaa264486467f,
+    "69fe92265705136a": _69fe92265705136a,
+    "88a0cf903b3fae98": _88a0cf903b3fae98,
+    "102a44c26518d92f": _102a44c26518d92f,
+    "141a44a63c4d9079": _141a44a63c4d9079,
+    "232ed32d07cb116a": _232ed32d07cb116a,
+    "0347f1d09f2bfbb8": _0347f1d09f2bfbb8,
+    "611d0f1df4e6cc92": _611d0f1df4e6cc92,
+    "1383d5dd58cdf971": _1383d5dd58cdf971,
+    "02096d9d171a9f85": _02096d9d171a9f85,
+    "3016ef8b1b8d8ba1": _3016ef8b1b8d8ba1,
+    "3763da893380788e": _3763da893380788e,
+    "39086c76adecb8ba": _39086c76adecb8ba,
+    "87385e3763cfd6e5": _87385e3763cfd6e5,
+    "97620ae6d0cecefa": _97620ae6d0cecefa,
+    "4019956e289995bf": _4019956e289995bf,
+    "5176864f54f7c989": _5176864f54f7c989,
+    "7827252d71dd26be": _7827252d71dd26be,
+    "a16a7ef790ddf8b5": _a16a7ef790ddf8b5,
+    "b0b22b0235a4d1ef": _b0b22b0235a4d1ef,
+    "b8a61184c56b908b": _b8a61184c56b908b,
+    "b056a7102f9a5d80": _b056a7102f9a5d80,
+    "bef85d570a643bfc": _bef85d570a643bfc,
+    "c8f4ee81076ab36a": _c8f4ee81076ab36a,
+    "d0e5844fffb28bf5": _d0e5844fffb28bf5,
+    "d9a40836b8b1e98d": _d9a40836b8b1e98d,
+    "ecf1faabfd42c507": _ecf1faabfd42c507,
+    "f0a91b97e78f59d5": _f0a91b97e78f59d5,
+};
